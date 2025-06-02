031a64def1a02d46c49608b5409d416b4625788b
Dhater <leandronorambuena1802@gmail.com>
2025-06-01 23:12:18 -0400

    Actualizados docker-compose, start.sh para corregir errores de pig y el git ignore para los resultados

--- Filtro/scripts/start.sh
+++ Filtro/scripts/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 /usr/sbin/sshd
+
+# Borrar la carpeta /pig/output antes de ejecutar
+rm -rf /pig/output
+
 mkdir -p /pig/input
+
 pig -x local /pig/scripts/filtro.pig || echo 'Falló Pig filtro'
 pig -x local /pig/scripts/analizar.pig || echo 'Falló Pig análisis'
+
 tail -f /dev/null
